fd91d5199a3d02e2988cc9025343ace33876494b
Nikhil Tiwari <nikhiltiwari1903@gmail.com>
2022-01-13 21:31:45 +0530

    added initial files

---
+++ placeorder.py
@@ -0,0 +1,21 @@
+def place_order(token,symbol,qty,buy_sell,ordertype,price,variety= 'NORMAL',exch_seg='NSE',triggerprice=0):
+    try:
+        orderparams = {
+            "variety": variety,
+            "tradingsymbol": symbol,
+            "symboltoken": token,
+            "transactiontype": buy_sell,
+            "exchange": exch_seg,
+            "ordertype": ordertype,
+            "producttype": "INTRADAY",
+            "duration": "DAY",
+            "price": price,
+            "squareoff": "0",
+            "stoploss": "0",
+            "quantity": qty,
+            "triggerprice":triggerprice
+            }
+        orderId=credentials.SMART_API_OBJ.placeOrder(orderparams)
+        print("The order id is: {}".format(orderId))
+    except Exception as e:
+        print("Order placement failed: {}".format(e.message))
